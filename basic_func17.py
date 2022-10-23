1aaf71e13dfc389870a372752692b14acc0afe2f
Akhmad <akhmadhurshetov@gmail.com>
2022-10-24 04:13:46 +0500

    17

--- basic_func17.py
+++ basic_func17.py
@@ -1,3 +1,9 @@
+from math import pi
+
+def main(a):
+    a = pi
+    return a
+
 # Create a function called main.
 # Create function arguments a.
 # Assign the value pi to the variable "a" and return.
