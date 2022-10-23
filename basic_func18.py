b0a67f6deb3f32e5eb9436cf973fd309ee3587a9
Akhmad <akhmadhurshetov@gmail.com>
2022-10-24 04:15:29 +0500

    18

--- basic_func18.py
+++ basic_func18.py
@@ -1,3 +1,9 @@
+from math import pi
+
+def main(a):
+    a = pi
+    return(round(a, 2))
+
 # Create a function called main.
 # Create function arguments a.
 # Assign the value pi to the variable "a" and return.
