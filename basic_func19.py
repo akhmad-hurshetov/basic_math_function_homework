b100f75682481b9d0ab7034b63cee7395ac0def7
Akhmad <akhmadhurshetov@gmail.com>
2022-10-24 04:25:39 +0500

    19

--- basic_func19.py
+++ basic_func19.py
@@ -1,3 +1,7 @@
+def main(a, b):
+    c = a - b
+    return (c**2)**(1/2)
 # Create a function called main.
 # Create function arguments a and b.
 # Return the absolute value of the difference between a and b.
+print(main(4, 11))
